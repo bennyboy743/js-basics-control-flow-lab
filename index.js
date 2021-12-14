2405b40624a28059d6d1a247dd15a32e9968bfc3
Benjamin <benjaminrowe0@gmail.com>
2021-12-14 18:29:35 +1100

    commit tree 720878b5dc8b56dc360a53c56fe94addf0e56395

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+const maxRide = 2500
+const minRide = 400
+
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride < minRide ){
+    return "This one is on me!"
+  }else if (ride >  2000 && ride <= maxRide){
+    return "I will gladly take your thirty bucks."
+  }else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city = city === "NYC" ? `Ok, sounds good.` : `No go.`
+  return city 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(wordTip){
   // Write your code here!
+  switch(wordTip){
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+  }
 }
